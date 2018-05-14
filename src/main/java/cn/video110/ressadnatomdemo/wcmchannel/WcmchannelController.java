7ebd393b8b78cd5b0b89cc617b01388500a35a66
717457836@qq.com <717457836@qq.com>
2018-05-14 17:20:47 +0800

    最终定稿

--- src/main/java/cn/video110/ressadnatomdemo/wcmchannel/WcmchannelController.java
+++ src/main/java/cn/video110/ressadnatomdemo/wcmchannel/WcmchannelController.java
@@ -20,6 +20,11 @@ public class WcmchannelController {
         return wcmchannelService.findBySITEIDAndSTATUSAndPARENTIDAndCHNLDATAPATHIsNotNull(4, 0, 0);
     }
 
+    /**
+     *
+     * @param docchannelId
+     * @return
+     */
     @GetMapping("/wcmchannel/channel")
     public Channel rss(Integer docchannelId) {
         Channel channel = wcmchannelService.packageChanl(docchannelId);
